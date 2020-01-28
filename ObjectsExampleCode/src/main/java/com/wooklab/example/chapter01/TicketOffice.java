122508538ed1007a6b21737be7f0986f91a18085
Hwang Inwook <inwook9@gmail.com>
2020-01-28 23:49:37 +0900

    <Chapter 01> 객체,설계 - 오타수정

--- ObjectsExampleCode/src/main/java/com/wooklab/example/chapter01/TicketOffice.java
+++ ObjectsExampleCode/src/main/java/com/wooklab/example/chapter01/TicketOffice.java
@@ -1,6 +1,7 @@
 package com.wooklab.example.chapter01;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -12,9 +13,9 @@ public class TicketOffice {
     /** 판매하거나 교환해 줄 티켓의 목록 */
     private List<Ticket> tickets = new ArrayList<>();
 
-    public TicketOffice(Long amount, List<Ticket> tickets) {
+    public TicketOffice(Long amount, Ticket ... tickets) {
         this.amount = amount;
-        this.tickets = tickets;
+        this.tickets.addAll(Arrays.asList(tickets));
     }
 
     public void sellTicketTo(Audience audience) {
